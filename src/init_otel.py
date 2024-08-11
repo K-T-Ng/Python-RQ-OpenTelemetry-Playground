a2053aa62da175abc1a9d70d5be33d91e53eee6a
K-T-Ng <l19so18h13l10@gmail.com>
2024-08-12 00:32:33 +0800

    init opentelemetry for rq

---
+++ src/init_otel.py
@@ -0,0 +1,53 @@
+"""Initalize OpenTelemetry instrumentation for logs/metrics/logs"""
+
+import logging
+
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor, ConsoleLogExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+
+from opentelemetry import _logs, metrics, trace
+
+
+def init_traces():
+    """Initalize traces instrumentation"""
+    provider = TracerProvider()
+    processor = BatchSpanProcessor(ConsoleSpanExporter())
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
+
+
+def init_meter():
+    """Initialize metric instrumentation"""
+    metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
+    provider = MeterProvider(metric_readers=[metric_reader])
+    metrics.set_meter_provider(provider)
+
+
+def init_logs():
+    """Initialize logs instrumentation"""
+    provider = LoggerProvider()
+    processor = BatchLogRecordProcessor(ConsoleLogExporter())
+    provider.add_log_record_processor(processor)
+    _logs.set_logger_provider(provider)
+
+
+def get_logger():
+    """Get logger (logging.getLogger(__name__)) with logs provider injected as handler"""
+    logger = logging.getLogger(__name__)
+    provier = _logs.get_logger_provider()
+    handler = LoggingHandler(level=logging.NOTSET, logger_provider=provier)
+    logger.addHandler(handler)
+    return logger
+
+
+def init_otel():
+    init_logs()
+    init_meter()
+    init_traces()
--- src/worker.py
+++ src/worker.py
@@ -1,9 +1,17 @@
 """RQ worker"""
 
+import logging
+
 from redis import Redis
 from rq import Queue, Worker
 
+from src.init_otel import get_logger, init_otel
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.NOTSET)
+    init_otel()
+    logger = get_logger()
+
     redis = Redis(host="localhost", port=6379)
     queue = Queue("task_queue", connection=redis)
 
