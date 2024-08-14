6a60a3560921dc0aa7817c397fed560cec107e9e
K-T-Ng <l19so18h13l10@gmail.com>
2024-08-14 23:39:49 +0800

    refactor: RQ instrumentor

--- opentelemetry/instrumentation/rq/__init__.py
+++ opentelemetry/instrumentation/rq/__init__.py
@@ -2,20 +2,84 @@
 A library that instrument `rq` library for learning aspect.
 """
 
-from typing import Collection
+import logging
+from typing import Callable, Collection, Dict, Tuple, Union
 
+import rq.job
 import rq.queue
 import rq.worker
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.utils import unwrap
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from rq.job import Job
+from rq.queue import Queue
+from rq.worker import Worker
 from wrapt import wrap_function_wrapper
 
+from opentelemetry import _logs, trace
 from opentelemetry.instrumentation.rq.package import _instruments
 from opentelemetry.instrumentation.rq.utils import (
     _add_handler_with_provider_to_logger,
-    _trace__enqueue_job,
-    _trace_perform_job,
+    _extract_attributes,
+    _extract_context_from_job_meta,
+    _inject_context_to_job_meta,
+    _remove_handler_with_provider_from_logger,
 )
+
+LOGGERS = ("rq.job", "rq.queue", "rq.registry", "rq.worker")
+
+
+def _instrument_execute(force_flush: bool = False) -> Callable:
+    """Instrument execute methods
+    rq.worker.BaseWorker.execute_job(job, queue)
+    rq.worker.Worker.perform_job(job, queue)
+    job.perform()
+    """
+
+    def _inner(func: Callable, instance: Union[Job, Worker], args: Tuple, kwargs: Dict):
+        """Instrument execution function"""
+        if isinstance(instance, Worker):
+            job, queue = args[0], args[1]
+        else:
+            job, queue = instance, None
+
+        attributes: Dict = _extract_attributes(job=job, queue=queue)
+        ctx: trace.Context = _extract_context_from_job_meta(job)
+
+        tracer = trace.get_tracer(__name__)
+        with tracer.start_as_current_span(
+            name=func.__name__, kind=trace.SpanKind.CONSUMER, context=ctx
+        ) as span:
+            if span.is_recording():
+                span.set_attributes(attributes=attributes)
+
+            response = func(*args, **kwargs)
+
+        if force_flush:
+            trace.get_tracer_provider().force_flush()
+            _logs.get_logger_provider().force_flush()
+
+        return response
+
+    return _inner
+
+
+def _instruement_enqueue(func: Callable, instance: Queue, args: Tuple, kwargs: Dict):
+    """Instrument rq.queue.Queue._queue_job"""
+    job: Job = args[0]
+    queue: Queue = instance
+    attributes: Dict = _extract_attributes(job=job, queue=queue)
+
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span(
+        name="enqueue", kind=trace.SpanKind.PRODUCER
+    ) as span:
+        if span.is_recording():
+            span.set_attributes(attributes=attributes)
+            _inject_context_to_job_meta(job)
+        response = func(*args, **kwargs)
+
+    return response
 
 
 class RQInstrumentor(BaseInstrumentor):
@@ -23,10 +87,22 @@ class RQInstrumentor(BaseInstrumentor):
         return _instruments
 
     def _instrument(self, **kwargs):
-        wrap_function_wrapper("rq.queue", "Queue._enqueue_job", _trace__enqueue_job)
-        wrap_function_wrapper("rq.worker", "Worker.perform_job", _trace_perform_job)
-        _add_handler_with_provider_to_logger()
+        self.log_provider: LoggerProvider = _logs.get_logger_provider()
+        self.log_handler = LoggingHandler(
+            level=logging.NOTSET, logger_provider=self.log_provider
+        )
+        _add_handler_with_provider_to_logger(LOGGERS, self.log_handler)
+
+        wrap_function_wrapper("rq.queue", "Queue._enqueue_job", _instruement_enqueue)
+        wrap_function_wrapper("rq.job", "Job.perform", _instrument_execute())
+        wrap_function_wrapper(
+            "rq.worker", "Worker.perform_job", _instrument_execute(force_flush=True)
+        )
+        wrap_function_wrapper("rq.worker", "Worker.execute_job", _instrument_execute())
 
     def _uninstrument(self, **kwargs):
-        unwrap(rq.worker.Worker, "perform_job")
         unwrap(rq.queue.Queue, "_enqueue_job")
+        unwrap(rq.worker.Worker, "execute_job")
+        unwrap(rq.worker.Worker, "perform_job")
+        unwrap(rq.job.Job, "perform")
+        _remove_handler_with_provider_from_logger(LOGGERS, self.log_handler)
--- opentelemetry/instrumentation/rq/utils.py
+++ opentelemetry/instrumentation/rq/utils.py
@@ -1,65 +1,57 @@
 """Utils for supporting PoC RQ instrumentation library"""
 
 import logging
+from typing import Optional, Tuple
 
-from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs import LoggingHandler
 from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from rq.job import Job
 from rq.queue import Queue
 from rq.worker import Worker
 
-from opentelemetry import _logs, trace
+from opentelemetry import trace
 
-LOGGERS = ["rq.job", "rq.queue", "rq.registry", "rq.worker"]
+
+def _extract_attributes(
+    job: Optional[Job] = None,
+    queue: Optional[Queue] = None,
+    worker: Optional[Worker] = None,
+):
+    """Extract attributes for instrumentation from job, queue and worker
+    Skip corresponding attributes if not provided
+    """
+    attributes = {}
+
+    if job:
+        attributes["job.id"] = job.id
+        attributes["job.func_name"] = job.func_name
+
+    if queue:
+        attributes["queue.name"] = queue.name
+
+    if worker:
+        attributes["worker.name"] = worker.name
+
+    return attributes
 
 
 def _inject_context_to_job_meta(job: Job):
     TraceContextTextMapPropagator().inject(job.meta)
 
 
-def _extract_context_from_job_meta(job: Job):
-    TraceContextTextMapPropagator().extract(carrier=job.meta)
+def _extract_context_from_job_meta(job: Job) -> trace.Context:
+    return TraceContextTextMapPropagator().extract(carrier=job.meta)
 
 
-def _trace__enqueue_job(func, instance, args, kwargs):
-    job: Job = args[0]
-    queue: Queue = instance
-
-    tracer = trace.get_tracer(__name__)
-    with tracer.start_as_current_span(
-        name=func.__name__, kind=trace.SpanKind.PRODUCER
-    ) as span:
-        if span.is_recording():
-            span.set_attribute("job_id", job.get_id())
-            span.set_attribute("queue_name", queue.name)
-        _inject_context_to_job_meta(job)
-        response = func(*args, **kwargs)
-    return response
+def _add_handler_with_provider_to_logger(
+    logger_names: Tuple[str], handler: LoggingHandler
+):
+    for name in logger_names:
+        logging.getLogger(name).addHandler(handler)
 
 
-def _trace_perform_job(func, instance, args, kwargs):
-    job: Job = args[0]
-    queue: Queue = args[1]
-    worker: Worker = instance
-
-    tracer = trace.get_tracer(__name__)
-    ctx = TraceContextTextMapPropagator().extract(job.meta)
-    with tracer.start_as_current_span(
-        name="perform_job", kind=trace.SpanKind.CONSUMER, context=ctx
-    ) as span:
-        if span.is_recording():
-            span.set_attribute("job.id", job.get_id())
-            span.set_attribute("queue_name", queue.name)
-            span.set_attribute("worker_name", worker.name)
-        response = func(*args, **kwargs)
-
-    trace.get_tracer_provider().force_flush()
-    return response
-
-
-def _add_handler_with_provider_to_logger():
-    provider: LoggerProvider = _logs.get_logger_provider()
-    handler = LoggingHandler(level=logging.NOTSET, logger_provider=provider)
-    for name in LOGGERS:
-        logger = logging.getLogger(name)
-        logger.addHandler(handler)
+def _remove_handler_with_provider_from_logger(
+    logger_names: Tuple[str], handler: LoggingHandler
+):
+    for name in logger_names:
+        logging.getLogger(name).removeHandler(handler)
